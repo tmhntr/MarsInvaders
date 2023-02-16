aed7a1a468857a67ccb2abdc86995504a97f045c
Tim Hunter <thunte27@uwo.ca>
2023-02-16 18:50:20 -0500

    add foe to game

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import pygame
+import random
 from pygame.locals import *
+
+class Foe(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygame.image.load('Assets/alien-2.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
 #initilze the pygame
 pygame.init()
 
@@ -15,12 +25,25 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load('Assets/spaceship.png')
 playerX = 370
 playerY = 480
+playerX_change = 0
+playerY_change = 0
 
-def player():
-    screen.blit(playerImg, (playerX, playerY)) #blit means to draw
+#Foe
+foeImg = pygame.image.load('Assets/alien-2.png')
+foeX = random.randint(0, 800)
+foeY = random.randint(50, 150)
+foeX_change = 0.3
+foeY_change = 10
+
+def player(x, y):
+    screen.blit(playerImg, (x, y)) #blit means to draw
+
+def foe(x, y):
+    screen.blit(foeImg, (x, y)) #blit means to draw
 
 #Game loop
 running = True
+speed = 0.5
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -29,16 +52,51 @@ while running:
             if event.key == pygame.K_ESCAPE:
                 running = False
             if event.key == pygame.K_RIGHT:
-                playerX += 40
+                playerX_change = speed
             if event.key == pygame.K_LEFT:
-                playerX -= 40
+                playerX_change = -speed
             if event.key == pygame.K_UP:
-                playerY -= 40
+                playerY_change = -speed
             if event.key == pygame.K_DOWN:
-                playerY += 40
+                playerY_change = speed
+        if event.type == pygame.KEYUP:
+            #keystroke has been released
+            if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT or event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                playerX_change = 0
+                playerY_change = 0
 
     #RGB - Red, Green, Blue
     screen.fill((75, 32, 16))
 
-    player()
+    #5 = 5 + -0.1 -> 5 = 5 - 0.1
+    #5 = 5 + 0.1
+
+    #checking for boundaries of spaceship so it doesn't go out of bounds
+    playerX += playerX_change
+
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 736: #subtracting 64 from 800
+        playerX = 736
+
+    playerY += playerY_change
+
+    if playerY <= 0:
+        playerY = 0
+    elif playerY >= 536: #subtracting 64 from 600
+        playerY = 536
+
+    # foe movement and boundaries
+    foeX += foeX_change
+    
+    if foeX <= 0:
+        foeX_change = 0.3
+        foeY += foeY_change
+    elif foeX >= 736: #subtracting 64 from 800
+        foeX_change = -0.3
+        foeY += foeY_change
+    
+
+    player(playerX, playerY)
+    foe(foeX, foeY)
     pygame.display.flip()
