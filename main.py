7b07cc271fa9d2f7dbe5238a91a6a0b6233c5029
Tim Hunter <thunte27@uwo.ca>
2023-02-16 17:48:11 -0500

    add movement

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ playerImg = pygame.image.load('Assets/spaceship.png')
 playerX = 370
 playerY = 480
 
+def player():
+    screen.blit(playerImg, (playerX, playerY)) #blit means to draw
+
 #Game loop
 running = True
 while running:
@@ -25,8 +28,17 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
+            if event.key == pygame.K_RIGHT:
+                playerX += 40
+            if event.key == pygame.K_LEFT:
+                playerX -= 40
+            if event.key == pygame.K_UP:
+                playerY -= 40
+            if event.key == pygame.K_DOWN:
+                playerY += 40
 
     #RGB - Red, Green, Blue
     screen.fill((75, 32, 16))
 
+    player()
     pygame.display.flip()
